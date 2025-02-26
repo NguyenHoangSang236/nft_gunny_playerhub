738dc932bd91792c94984291e077655fc9018054
Thanh Thien <thien.mlt8601@sinhvien.hoasen.edu.vn>
2025-02-26 20:35:00 +0700

    GetCharacterById

--- src/main/java/com/nftgunny/playerhub/infrastructure/repository/CharacterRepository.java
+++ src/main/java/com/nftgunny/playerhub/infrastructure/repository/CharacterRepository.java
@@ -2,9 +2,13 @@ package com.nftgunny.playerhub.infrastructure.repository;
 
 import com.nftgunny.playerhub.entities.database.Character;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CharacterRepository extends MongoRepository<Character, Integer> {
+import java.util.Optional;
 
+@Repository
+public interface CharacterRepository extends MongoRepository<Character, String> {
+    @Query("{'characterId': ?0}")
+    Optional<Character> findCharacterById(String id);
 }
--- src/main/java/com/nftgunny/playerhub/usecases/character/EditCharacterUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.nftgunny.playerhub.usecases.character;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class EditCharacterUseCase {
-
-}
