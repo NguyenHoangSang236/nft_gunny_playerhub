6d0f7ed5f9b5206287e070602a713cd3c5aaf16d
Thanh Thien <thien.mlt8601@sinhvien.hoasen.edu.vn>
2025-02-26 20:35:00 +0700

    GetCharacterById

--- src/main/java/com/nftgunny/playerhub/infrastructure/controller/CharacterController.java
+++ src/main/java/com/nftgunny/playerhub/infrastructure/controller/CharacterController.java
@@ -1,12 +1,22 @@
 package com.nftgunny.playerhub.infrastructure.controller;
 
 import com.nftgunny.core.common.usecase.UseCaseExecutor;
+import com.nftgunny.core.entities.api.response.ApiResponse;
+import com.nftgunny.core.entities.api.response.ResponseMapper;
+import com.nftgunny.playerhub.usecases.character.GetCharacterByIdUseCase;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
 
 @Validated
 @Tag(name = "Character", description = "Operations relating to managing character")
@@ -15,6 +25,19 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class CharacterController {
     final UseCaseExecutor useCaseExecutor;
+    final GetCharacterByIdUseCase getCharacterByIdUseCase;
 
+    @Operation(summary = "Get character infomation by character ID")
+    @GetMapping("/charater/{id}")
+    public CompletableFuture<ResponseEntity<ApiResponse>> getCharacterById(
+            @PathVariable("id")
+            @NotBlank String characterId
+    ){
+    return useCaseExecutor.execute(
+            getCharacterByIdUseCase,
+            new GetCharacterByIdUseCase.InputValue(characterId),
+            ResponseMapper::map
+    );
+    }
 
 }
---
+++ src/main/java/com/nftgunny/playerhub/usecases/character/GetCharacterByIdUseCase.java
@@ -0,0 +1,45 @@
+package com.nftgunny.playerhub.usecases.character;
+
+import com.nftgunny.core.common.usecase.UseCase;
+import com.nftgunny.core.entities.api.response.ApiResponse;
+import com.nftgunny.playerhub.entities.database.Character;
+import com.nftgunny.playerhub.infrastructure.repository.CharacterRepository;
+import com.nftgunny.playerhub.usecases.user.LoginUseCase;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class GetCharacterByIdUseCase extends UseCase<GetCharacterByIdUseCase.InputValue, ApiResponse> {
+
+    final CharacterRepository characterRepository;
+
+    public GetCharacterByIdUseCase(CharacterRepository characterRepository) {
+        this.characterRepository = characterRepository;
+    }
+
+    @Override
+    public ApiResponse execute(InputValue input) {
+        Optional<Character> characterOptional = characterRepository.findCharacterById(input.characterId());
+        if (characterOptional.isPresent()) {
+            Character character = characterOptional.get();
+            return ApiResponse.builder()
+                    .result("success")
+                    .message("Get character successfully")
+                    .content(character)
+                    .status(HttpStatus.OK)
+                    .build();
+        }
+        else{
+            return ApiResponse.builder()
+                    .result("error")
+                    .message("Character not found")
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+    }
+
+    public record InputValue(String characterId) implements UseCase.InputValue {};
+}
+
